935f515704f263e06261663671bd25d350c80b5f
Stefano Bielli <stefano.bielli@gmail.com>
2018-07-22 00:15:44 +0200

    lesson 14

--- web/src/com/airline/controllers/AddPassenger.java
+++ web/src/com/airline/controllers/AddPassenger.java
@@ -1,6 +1,9 @@
 package com.airline.controllers;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,37 +12,55 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.airline.models.Gender;
+
 /**
  * Servlet implementation class AddPassenger
  */
 @WebServlet("/AddPassenger")
 public class AddPassenger extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AddPassenger() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		RequestDispatcher view = request.getRequestDispatcher("WEB-INF/views/add_passenger.jsp");
-		
-		view.forward(request, response);
-		
+	public AddPassenger() {
+		super();
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			RequestDispatcher view = request.getRequestDispatcher("WEB-INF/views/add_passenger.jsp");
+			view.forward(request, response);
+		} catch (ServletException | IOException e) {
+			// well
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String firstName = request.getParameter("first-name");
+		String lastName = request.getParameter("last-name");
+		Date birthDate;
+		try {
+			birthDate = new SimpleDateFormat("dd/MM/yyyy").parse(request.getParameter("dob"));
+		} catch (ParseException e) {
+			// doh
+		}
+
+		Gender gender = Gender.valueOf(request.getParameter("gender").toUpperCase());
+
 	}
 
 }
